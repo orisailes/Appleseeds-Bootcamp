55a39688c2188412523dc044fae60b8079b2ab3c
Ori Sailes <ori1sailes@gmail.com>
2021-04-11 14:06:06 +0300

    file adding

---
+++ node.js/1.1/nodeVsJs.js
@@ -0,0 +1,11 @@
+// node.js have the "proccess" object, "global" object and "filesystem" functionality.
+// the proccess object provides information about the runtime of the program.
+// "global" obj is equivilant to the window obj in browser. "global" is the global scope itself and we can 
+// define variable and function to it.
+// "filesystem" is let us access to the file system 
+
+
+//* javascript client side has the "window" object, as well as the "document" object and the web apis.
+//* node js has no need those object because he handles things that is not reelevant to the client side.
+//* for example, "window" object can give us the width and height of the client screen. in the other hande, node js 
+//* doesnt need this information at all.
