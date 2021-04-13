c47acb9c2965dcb2c526d6258a311e567d2df36f
orisailes <ori1sailes@gmail.com>
2021-04-13 14:26:00 +0300

    file adding

---
+++ node.js/9.2/pureServer.js
@@ -0,0 +1,62 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const host = 'localhost';
+const port = 3000;
+
+const routingData = {
+    users: require("./users.json"),
+    rawHtml: "<h1>Welcome</h1>",
+    html: fs.readFileSync("./index.html"),
+    css: fs.readFileSync("./index.css"),
+    js: fs.readFileSync("./index.js"),
+    notFound: "page not found"
+
+}
+
+const requestListener = (req, res) => {
+    switch (req.url) {
+        case "/":
+            res.writeHead(200, {
+                'Content-Type': 'text/html',
+            })
+            res.write(routingData.html);
+            break;
+        case "/index.css":
+            res.writeHead(200, {
+                'Content-Type': 'text/css',
+            });
+            res.write(routingData.css)
+            break;
+        case "/index.js":
+            res.writeHead(200, {
+                'Content-Type': 'text/javascript',
+            });
+            res.write(routingData.js)
+            break;
+        case "/raw-html":
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            });
+            res.write(routingData.rawHtml);
+            break;
+        case "/users":
+            res.writeHead(200, {
+                'Content-Type': 'application/json'
+            });
+            res.write(JSON.stringify(routingData.users));
+            break;
+        default:
+            res.writeHead(400, {
+                'Content-Type': 'text/html'
+            });
+            res.write(routingData.notFound)
+            break;
+    }
+    res.end();
+}
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`Server is running at http://${host}:${port}`)
+})
