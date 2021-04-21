56bd1656f426b607c25a46a19e1a652fbb1080c7
orisailes <ori1sailes@gmail.com>
2021-04-21 15:19:35 +0300

    file adding

--- node.js/15.1/src/db/mongoose.js
+++ node.js/15.1/src/db/mongoose.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 const Product = require('../models/Product')
 
+
 mongoose.connect('mongodb://127.0.0.1:27017/myShop', {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify:false,
 })
 
--- node.js/15.1/src/index.js
+++ node.js/15.1/src/index.js
@@ -4,9 +4,10 @@ const Product = require('./models/Product')
 const app = express();
 const port = process.env.PORT || 5000;
 
+
 app.use(express.json());
 
-app.post('/product', async (req, res) => {
+app.post('api/product', async (req, res) => {
     const product = new Product(req.body);
     try {
         const result = await product.save();
@@ -17,8 +18,8 @@ app.post('/product', async (req, res) => {
 
 })
 
-app.get('/product', async (req, res) => {
-    if(!req.query){
+app.get('api/product', async (req, res) => {
+    if (!req.query) {
         try {
             const result = await Product.find({});
             res.send(result);
@@ -27,7 +28,7 @@ app.get('/product', async (req, res) => {
         }
     }
     if (req.query) {
-        if (Object.keys(req.query)[0] || Object.keys(req.query)[1] ===("isActive")) {
+        if (Object.keys(req.query)[0] || Object.keys(req.query)[1] === ("isActive")) {
             try {
                 const result = await Product.find(req.query);
                 console.log('result:', result)
@@ -40,19 +41,27 @@ app.get('/product', async (req, res) => {
 
 })
 
-app.get('/product/findByPriceRange',async(req,res)=>{
-    if(req.query){
-        const {min,max} = req.query
-        try{
-            const result = await Product.find({"details.price":{$gte:min,$lte:max}}) 
-            res.send(result)
-        }catch(err){
-            res.status(400).send(err.message)
+app.get('api/product/findByPriceRange', async (req, res) => {
+    if (req.query) {
+        const {
+            min,
+            max
+        } = req.query
+        try {
+            const result = await Product.find({
+                "details.price": {
+                    $gte: min,
+                    $lte: max
+                }
+            });
+            res.send(result);
+        } catch (err) {
+            res.status(400).send(err.message);
         }
     }
 })
 
-app.get('/product/:id', async (req, res) => {
+app.get('api/product/:id', async (req, res) => {
     const {
         id
     } = req.params;
@@ -62,8 +71,56 @@ app.get('/product/:id', async (req, res) => {
         });
         res.send(result);
     } catch (err) {
-        res.send(err.message);
+        res.status(400).send(err.message);
     }
+})
+
+app.put('/api/product/:id', async (req, res) => {
+    const {
+        id
+    } = req.params;
+
+    if (Object.keys(req.body).length) {
+        const allowOnly = ['name', '_id', 'category', 'isActive', 'details.discount', 'details.description', 'details.price', 'details.phone', 'details.img'];
+        const isValidUpdate = Object.keys(req.body).every(key => allowOnly.includes(key))
+        if (isValidUpdate) {
+            try {
+                const result = await Product.findByIdAndUpdate(id, req.body, {
+                    new: true,
+                    runValidators: true
+                });
+                if (!result) return res.status(400).send();
+                res.send(result);
+            } catch (err) {
+                res.status(400).send(err.message);
+            }
+        } else {
+            res.status(400).send('invalid update. please double check your update fields');
+        }
+    }
+
+
+    if (Object.keys(req.query).length) {
+        try {
+            let helper = {}
+            if (req.query.discount) {
+                helper["details.discount"] = req.query.discount
+            }
+            if (req.query.isActive) {
+                helper["isActive"] = req.query.isActive
+            }
+            console.log(helper);
+            const result = await Product.findByIdAndUpdate(id, helper, {
+                new: true,
+                runValidators: true
+            })
+            res.status(200).send(result)
+        } catch (err) {
+            res.status(400).send('invalid update. queries in invalid')
+        }
+    }
+
+    if(!Object.keys(req.query).length && !Object.keys(req.body).length) res.status(400).send('invalid put request')
 })
 
 app.listen(port, () => {
