d132404eb482678cec55985640acf76d14c75802
orisailes <ori1sailes@gmail.com>
2021-05-07 12:41:59 +0300

    file adding

--- end_course_project/client/src/utils/constants/constants.js
+++ end_course_project/client/src/utils/constants/constants.js
@@ -1,3 +1,3 @@
 
 export const SpriteSize = { width:26, height:34}
-export const MapSize = { width:SpriteSize.width*35, height:SpriteSize.height*15}
+export const MapSize = { width:SpriteSize.width*38, height:SpriteSize.height*15}
