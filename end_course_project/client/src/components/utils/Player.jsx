1bd8fb8523ffa4f284357eff726ff68ef5145807
orisailes <ori1sailes@gmail.com>
2021-05-10 14:29:26 +0300

    file adding

--- end_course_project/client/src/components/utils/Player.jsx
+++ end_course_project/client/src/components/utils/Player.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../../css/player.css'
-import WalkSprites from '../../img/character/movement/maleBigger.png'
+import WalkSprites from '../../img/character/movement/maleSprites.png'
 import { SpriteSize } from '../../utils/constants/constants'
 
 const Player = ({ position,forwardedRef }) => {
@@ -9,14 +9,13 @@ const Player = ({ position,forwardedRef }) => {
     const y = 50
     const x = 50
 
-
     return (
         <div
             style={{
                 top: `calc(${x}vh + ${position[1]}vh)`,
                 left: `calc(${y}vw + ${position[0]}vw)`,
                 backgroundImage: `url(${WalkSprites})`,
-                backgroundPosition: '0 0', //27px 50px for step ??
+                backgroundPosition: `${SpriteSize.width*0}px ${50*0}px`, //  X Y
                 height: `${SpriteSize.height}px`,
                 width: `${SpriteSize.width}px`,
 
