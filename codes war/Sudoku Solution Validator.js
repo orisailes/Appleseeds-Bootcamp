8149e376a73cc6767f8775f82c435fdf83a08578
orisailes <ori1sailes@gmail.com>
2021-05-18 11:47:54 +0300

    file adding

--- codes war/Sudoku Solution Validator.js
+++ codes war/Sudoku Solution Validator.js
@@ -1,29 +1,79 @@
 function validSolution(board) {
 
-    const getRow = (i, j) => {
-        return board[i]
+    //! handle zeros
+    const boxes = {
+        boxOne: [],
+        boxTwo: [],
+        boxThree: [],
+        boxFour: [],
+        boxFive: [],
+        boxSix: [],
+        boxSeven: [],
+        boxEight: [],
+        boxNine: [],
     }
 
-    const getLine = (i, j) => {
+    const getRow = (index) => {
+        const row = board[index]
+        if (row.find((num) => num === 0)) return false
+        return row
+    }
+
+    const getLine = (index) => {
         const lineArr = []
         for (let row = 0; row < board.length; row++) {
-            lineArr.push(board[row][j])
+            lineArr.push(board[row][index])
         }
+        if (lineArr.find((num) => num === 0)) return false
         return lineArr
     }
 
-    const getBlock = (i,j) => {
+    const checkForDuplicates = (arr) => {
+        const checker = new Set(arr)
+        return checker.size === arr.length ? true : false
+    }
+
+    for (let i = 0; i < board.length; i++) { // fill up the boxes
+        for (let j = 0; j < board.length; j++) {
+            if (i < 3) {
+                j < 3 ? boxes.boxOne.push(board[i][j]) :
+                    j < 6 ? boxes.boxTwo.push(board[i][j]) :
+                    boxes.boxThree.push(board[i][j])
+            }
+            if (i < 6 && i >= 3) {
+                j < 3 ? boxes.boxFour.push(board[i][j]) :
+                    j < 6 ? boxes.boxFive.push(board[i][j]) :
+                    boxes.boxSix.push(board[i][j])
+            }
+            if (i < 9 && i >= 6) {
+                j < 3 ? boxes.boxSeven.push(board[i][j]) :
+                    j < 6 ? boxes.boxEight.push(board[i][j]) :
+                    boxes.boxNine.push(board[i][j])
+            }
+        }
 
     }
 
-    const test = getLine(7, 8)
-    console.log('test:', test)
-    
+    for (let i = 0; i < board.length; i++) {
+        if (i === 0) {
+            const isValid = checkForDuplicates(getRow(i))
+            if (!isValid) return false
+        }
+
+        const isValid = checkForDuplicates(getLine(i))
+        if (!isValid) return false
+
+    }
+    for (let box in boxes) {
+        const isValid = checkForDuplicates(boxes[box])
+        if (!isValid) return false
+    }
+    return true
 }
 
 
 
-validSolution([
+const x = validSolution([
     [5, 3, 4, 6, 7, 8, 9, 1, 2],
     [6, 7, 2, 1, 9, 5, 3, 4, 8],
     [1, 9, 8, 3, 4, 2, 5, 6, 7],
@@ -35,14 +85,17 @@ validSolution([
     [3, 4, 5, 2, 8, 6, 1, 7, 9]
 ]) // true
 
-// validSolution([
-//     [5, 3, 4, 6, 7, 8, 9, 1, 2],
-//     [6, 7, 2, 1, 9, 0, 3, 4, 8],
-//     [1, 0, 0, 3, 4, 2, 5, 6, 0],
-//     [8, 5, 9, 7, 6, 1, 0, 2, 0],
-//     [4, 2, 6, 8, 5, 3, 7, 9, 1],
-//     [7, 1, 3, 9, 2, 4, 8, 5, 6],
-//     [9, 0, 1, 5, 3, 7, 2, 1, 4],
-//     [2, 8, 7, 4, 1, 9, 6, 3, 5],
-//     [3, 0, 0, 4, 8, 1, 1, 7, 9]
-// ]) // false
+const y = validSolution([
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 0, 3, 4, 8],
+    [1, 0, 0, 3, 4, 2, 5, 6, 0],
+    [8, 5, 9, 7, 6, 1, 0, 2, 0],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 0, 1, 5, 3, 7, 2, 1, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 0, 0, 4, 8, 1, 1, 7, 9]
+]) // false 
+
+console.log('x:', x)
+console.log('y:', y)
