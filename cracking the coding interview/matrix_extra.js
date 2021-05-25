6f4c6e3478ae179c163a051a2efdb1314027c1c2
orisailes <ori1sailes@gmail.com>
2021-05-25 17:18:33 +0300

    file adding

--- cracking the coding interview/matrix_extra.js
+++ cracking the coding interview/matrix_extra.js
@@ -1,13 +1,35 @@
 // from java course at open university 
 
 function isSink(matrix) {
-    let lineSum = 0
-    const zerosRow = matrix.findIndex((row, i) => row.every((element => element === 0))) // find if there is row that all are 0's  and return the row index
-    if (!zerosRow) return false
+    let snail = 0
+    let sumLine = 0
     for (let i = 0; i < matrix.length; i++) {
-        lineSum += matrix[i][zerosRow];
+        const rightElement = matrix[i][i + snail]
+        if (rightElement === 0 || rightElement === 1) {
+            if (rightElement === 1) {
+                snail = 0
+            } else {
+                snail++
+                i--
+            }
+        } else {
+
+            const isAllZeros = matrix[i].slice(0, i).every((num => num === 0)) //check for all previous row 
+            //finally, check for line
+            if (isAllZeros) {
+                for (let j = 0; j < matrix.length; j++) {
+                    const element = matrix[j][i];
+                    sumLine += element
+                }
+                if (sumLine === 5) {
+                    return true
+                } else {
+                    sumLine = 0
+                }
+            }
+        }
     }
-    return lineSum === 5 ? true : false
+    return false
 }
 
 
@@ -27,8 +49,18 @@ const matrixB = [
     [0, 1, 0, 1, 0, 1],
     [1, 0, 0, 0, 1, 0]
 ]
+const matrixC = [
+    [0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 1, 1, 1],
+    [0, 0, 0, 0, 1, 0],
+    [1, 1, 1, 1, 1, 1],
+    [0, 1, 0, 1, 1, 1],
+    [1, 0, 0, 0, 1, 1]
+]
 
 const a = isSink(matrixA)
 const b = isSink(matrixB)
+const c = isSink(matrixC)
 console.log('a:', a)
 console.log('b:', b)
+console.log('c:', c)
